41dbbdbf05e75dfe502f20aefcb5aabcfd89595f
Sumanta Das <sumantad827@gmail.com>
2022-10-27 01:07:41 +0530

    invention commit

---
+++ demo.py
@@ -0,0 +1,13 @@
+
+
+from functools import reduce
+#python fibonacci series print program
+
+userinput = int(input("how much fibonacci series : "))
+myl = [0, 1]
+for x in range(userinput):
+    df = myl[-1]+myl[-2]
+    myl.append(df)
+data = myl[0:-2]
+for y in data:
+    print(y)
